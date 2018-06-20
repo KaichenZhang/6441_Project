82874818b58b72c3cc7b166278fbfedd07c994e0
Kaichen Zhang <kaichenzhang@hotmail.com>
2018-06-20 17:15:49 -0400

    [Add] Coding Practices 4

---
+++ Code Practices/Java and AspectJ/Interface Name Collisions/src/IF1.java
@@ -0,0 +1,7 @@
+// C. Constantinides, 2009.
+
+public interface IF1 {
+	void method1(int i);
+	void method2 (int i);
+//	void method3 (int i);
+}
---
+++ Code Practices/Java and AspectJ/Interface Name Collisions/src/IF2.java
@@ -0,0 +1,7 @@
+// C. Constantinides, 2009.
+
+public interface IF2 {
+		void method1(double d);
+		void method2 (int i);
+//		double method3 (int i);
+}
